5e043e42214ecc4d06fb4f496e1e17d8cb0db216
Levy Borromeo <mart_inc@outlook.com>
2025-09-11 10:05:02 +1200

    Create Remediation_WN10-CC-000175.ps1

---
+++ Remediation_WN10-CC-000175.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+This script ensures that the Application Compatibility Program Inventory is prevented from collecting data and sending the information to Microsoft.
+
+.NOTES
+    Author          : Levy Borromeo
+    LinkedIn        : linkedin.com/in/levyborromeo/
+    GitHub          : github.com/levyborromeo
+    Date Created    : 2025-09-11
+    Last Modified   : 2025-09-11
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000175
+
+.TESTED ON
+    Date(s) Tested  : 2025-09-11
+    Tested By       : Levy Borromeo
+    Systems Tested  : Windows 10 Pro VM 
+    PowerShell Ver. : PowerShell Version: 5.1.19041.6328
+
+.USAGE
+    PS C:\> .\Remediation_WN10-CC-000175.ps1
+#>
+
+# Define registry path
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"
+
+# Ensure the registry path exists
+If (!(Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force
+}
+
+# Set DisableInventory to 1 (Turn off Inventory Collector)
+Set-ItemProperty -Path $RegPath -Name "DisableInventory" -Value 1 -Type DWord
+
+# Force Group Policy update to apply changes
+gpupdate /force
+
+# Verify the configuration
+Get-ItemProperty -Path $RegPath -Name "DisableInventory"
